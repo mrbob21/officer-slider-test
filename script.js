f5ef48fbe2387e79c5b9dab77e9aa1ffdcbaae4d
mrbob21 <enoch.amat@gmail.com>
2023-09-25 17:02:14 -0500

    Temporarily leaving, finding another slider

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 document.getElementById('next').onclick = function(){
     let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);
+    document.getElementById('slide-one').appendChild(lists[0]);
 }
 document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').prepend(lists[lists.length - 1]);
+    document.getElementById('slide-one').prepend(lists[lists.length - 1]);
 }
+
+/*
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide-two').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide-two').prepend(lists[lists.length - 1]);
+}
+*/
